25a766218bb3547d68bd229eb37c8badaddc5f27
Bob Brown <robertbernardbrown@gmail.com>
2018-02-24 16:40:19 -0500

    adds server code to server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const mysql = require("mysql");
+// const connection = mysql.createConnection({
+//   host     : "localhost",
+//   user     : "root",
+//   password : "root",
+//   database : "bamazon_db",
+//   port: 8889
+// });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
