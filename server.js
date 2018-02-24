0f9b896f18806a023e7c60bee48e7f59dc8b4bb9
Bob Brown <robertbernardbrown@gmail.com>
2018-02-24 17:37:49 -0500

    stand-in text for survey and home pages. Sets up routing info in server file

--- server.js
+++ server.js
@@ -11,10 +11,19 @@ const path       = require("path");
 // });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + "/app/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
