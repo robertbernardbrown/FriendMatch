220f1c8eb7e1d1f5cdcd3ca4d71f32d98d34d5dd
Bob Brown <robertbernardbrown@gmail.com>
2018-02-27 14:48:49 -0500

    adds all db accessing code to friends.js file. Also fixes matcher to keep people from matching with selves

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,24 +7,55 @@ const connection = mysql.createConnection({
   port: 8889
 });
 
-let friendArr = [];
-let existingFriends = [];
-
 module.exports = {
-  friendArr: friendArr,
-  existingFriends: existingFriends,
-  existing: function(arr) {
-    connection.connect(function () {
-      fillExisting(arr);
+
+  connect: connection.connect(),
+
+  displayFriendData: function displayFriendData (callback) {
+    let friendArr=[];
+    connection.query("SELECT * FROM friend JOIN friend_responses ON friend_responses.friend_id = friend.friend_id", (err, res) => {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        var element = res[i];
+        friendArr.push(element);
+      }
+      callback(friendArr);
     });
-      
-    function fillExisting (arr) {
-      connection.query("SELECT * FROM friend JOIN friend_responses ON friend_responses.friend_id = friend.friend_id", function (error, results) {
-        for (var i = 0; i < results.length; i++) {
-          var element = results[i];
-          arr.push(element);
-        }
-      });
-    }
+  },
+
+  displayMatchFriendData: function displayMatchFriendData (callback, index) {
+    let friendArr=[];
+    connection.query("SELECT * FROM friend JOIN friend_responses ON friend_responses.friend_id = friend.friend_id WHERE friend.friend_id = ?",[index], (err, res) => {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        var element = res[i];
+        friendArr.push(element);
+      }
+      callback(friendArr);
+    });
+  },
+
+  addFriendToDb: function addFriendToDb (friendName, friendPic) {
+    connection.query("INSERT INTO friend SET friend_name=?, friend_pic=?",
+      [friendName, friendPic]);
+  },
+
+  addFriendValuesToDb: function addFriendValuesToDb (answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10) {
+    connection.query("INSERT INTO friend_responses SET question_01=?, question_02=?, question_03=?, question_04=?, question_05=?, question_06=?, question_07=?, question_08=?, question_09=?, question_10=?",
+      [answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10]);
+  },
+
+  fetchFriendResponses: function fetchFriendResponses(callback) {
+    connection.query("SELECT * FROM friend_responses", (err, res) => {
+      if (err) throw err;
+      callback(res);
+    });
+  },
+
+  addToDB: function addToDb(friendName, friendPic, answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10) {
+    connection.query("INSERT INTO friend SET friend_name=?, friend_pic=?",
+      [friendName, friendPic]);
+    connection.query("INSERT INTO friend_responses SET question_01=?, question_02=?, question_03=?, question_04=?, question_05=?, question_06=?, question_07=?, question_08=?, question_09=?, question_10=?",
+      [answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10]);
   }
 };
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,75 +1,21 @@
-const mysql      = require("mysql");
-const connection = mysql.createConnection({
-  host     : "localhost",
-  user     : "root",
-  password : "root",
-  database : "friendMatch_db",
-  port: 8889
-});
-// const friendArr = require("../data/friends").friendArr;
-// const existingFunc = require("../data/friends").existing;
-// const existingArr = require("../data/friends").existingFriends;
-// existingFunc(existingArr);
-
-function displayFriendData (callback) {
-  let friendArr=[];
-  connection.query("SELECT * FROM friend JOIN friend_responses ON friend_responses.friend_id = friend.friend_id", (err, res) => {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      var element = res[i];
-      friendArr.push(element);
-    }
-    callback(friendArr);
-  });
-}
-
-function displayMatchFriendData (callback, index) {
-  let friendArr=[];
-  connection.query("SELECT * FROM friend JOIN friend_responses ON friend_responses.friend_id = friend.friend_id WHERE friend.friend_id = ?",[index], (err, res) => {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      var element = res[i];
-      friendArr.push(element);
-    }
-    callback(friendArr);
-  });
-}
-
-function addFriendToDb (friendName, friendPic) {
-  connection.query("INSERT INTO friend SET friend_name=?, friend_pic=?",
-    [friendName, friendPic]);
-}
-
-function addFriendValuesToDb (answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10) {
-  connection.query("INSERT INTO friend_responses SET question_01=?, question_02=?, question_03=?, question_04=?, question_05=?, question_06=?, question_07=?, question_08=?, question_09=?, question_10=?",
-    [answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10]);
-}
-
-function fetchFriendResponses(callback) {
-  connection.query("SELECT * FROM friend_responses", (err, res) => {
-    if (err) throw err;
-    callback(res);
-  });
-}
+const friendDBActions = require("../data/friends");
 
 module.exports = (app) => {
 
   app.get("/api/friends", (req, res) => {
-    displayFriendData(setData);
+    friendDBActions.connect;
+    friendDBActions.displayFriendData(setData);
     function setData (arr) {
       res.json(arr);
     }
   });
   
   app.post("/api/friends", (req, res) => {
+    friendDBActions.connect;
     let newFriend = req.body;
-    connection.connect(()=> {
-      addFriendToDb(newFriend.friend_name, newFriend.friend_pic);
-      addFriendValuesToDb(newFriend.question_01,  newFriend.question_02, newFriend.question_03,  newFriend.question_04, newFriend.question_05, newFriend.question_06, newFriend.question_07, newFriend.question_08, newFriend.question_09, newFriend.question_10);
-    });
     res.json(newFriend);
     let newFriendVals = [0, newFriend.question_01,  newFriend.question_02, newFriend.question_03,  newFriend.question_04, newFriend.question_05, newFriend.question_06, newFriend.question_07, newFriend.question_08, newFriend.question_09, newFriend.question_10];
-    fetchFriendResponses(friendMatcher);
+    friendDBActions.fetchFriendResponses(friendMatcher);
     function friendMatcher (arr) {
       let index = 0;
       let compareVal = 100;
@@ -92,10 +38,11 @@ module.exports = (app) => {
           }
         }
       }
-      displayMatchFriendData(showData, index);
+      friendDBActions.displayMatchFriendData(showData, index);
       function showData (arr) {
         console.log(arr);
       }
+      friendDBActions.addToDB(newFriend.friend_name, newFriend.friend_pic, newFriend.question_01,  newFriend.question_02, newFriend.question_03,  newFriend.question_04, newFriend.question_05, newFriend.question_06, newFriend.question_07, newFriend.question_08, newFriend.question_09, newFriend.question_10);
     }
   });
 };
