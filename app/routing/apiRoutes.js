89a0b5a9d830a4a28e42df54540bd73d9abd1c6f
Bob Brown <robertbernardbrown@gmail.com>
2018-03-02 12:55:38 -0500

    saving before cleaning up the code just in case

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,35 +13,90 @@ module.exports = (app) => {
   app.post("/api/friends", (req, res) => {
     friendDBActions.connect;
     let newFriend = req.body;
-    let newFriendVals = [0, newFriend.q1,  newFriend.q2, newFriend.q3,  newFriend.q4, newFriend.q5, newFriend.q6, newFriend.q7, newFriend.q8, newFriend.q9, newFriend.q10];
+    console.log(newFriend);
+    let newFriendVals = 
+    [0, 
+      newFriend.q1,  
+      newFriend.q2, 
+      newFriend.q3,  
+      newFriend.q4, 
+      newFriend.q5, 
+      newFriend.q6, 
+      newFriend.q7, 
+      newFriend.q8, 
+      newFriend.q9, 
+      newFriend.q10];
     friendDBActions.fetchFriendResponses(friendMatcher);
     function friendMatcher (arr) {
-      let index = 0;
-      let compareVal = 100;
-      for (let i = 0; i < arr.length; i++) {
-        let element = arr[i];
-        let friendVals = Object.values(element);
-        let diffArr = [];
-        let difference;
-        for (var j = 1; j <= friendVals.length - 1; j++) {
-          let diff = Math.abs(friendVals[j] - newFriendVals[j]);
-          diffArr.push(diff);
-          if (j === friendVals.length - 1) {
-            difference = diffArr.reduce((accumulate, currVal) => {
-              return accumulate + currVal;
-            });
-            if (difference <= compareVal) {
-              compareVal = difference;
-              index = friendVals[0];
-            }
-          }
+
+      let objectToArray = object => {
+        let transformedArray = Object.values(object);
+        return transformedArray;
+      };
+
+      let arrayDifferences = arr => {
+        let diffArr=[];
+        for (var i = 0; i < newFriendVals.length; i++) {
+          var element = newFriendVals[i];
+          let thing = Math.abs(element - arr[i]);
+          diffArr.push(thing);
         }
-      }
+        return diffArr;
+      };
+
+      let arrTotals = (adder, currVal, index) => {
+        if (index < 0) {
+          return 0;
+        } else {
+          return adder + currVal;
+        }
+      };
+
+      
+
+      let dbFriendVals = arr.map(objectToArray).map(arrayDifferences);
+      //.map(arrayDifferences)
+      //.reduce(arrTotals);
+      // console.log(dbFriendVals);
+      // console.log(dbFriendVals);
+      // let index = 0;
+      // let compareVal = 100;
+      // for (let i = 0; i < arr.length; i++) {
+      //   let element = arr[i];
+      //   let friendVals = Object.values(element);
+      //   let diffArr = [];
+      //   let difference;
+      //   for (var j = 1; j <= friendVals.length - 1; j++) {
+      //     let diff = Math.abs(friendVals[j] - newFriendVals[j]);
+      //     diffArr.push(diff);
+      //     if (j === friendVals.length - 1) {
+      //       difference = diffArr.reduce((accumulate, currVal) => {
+      //         return accumulate + currVal;
+      //       });
+      //       if (difference <= compareVal) {
+      //         compareVal = difference;
+      //         index = friendVals[0];
+      //       }
+      //     }
+      //   }
+      // }
       friendDBActions.displayMatchFriendData(showData, index);
       function showData (arr) {
         res.json(arr);
       }
-      friendDBActions.addToDB(newFriend.friend_name, newFriend.friend_pic, newFriend.q1,  newFriend.q2, newFriend.q3,  newFriend.q4, newFriend.q5, newFriend.q6, newFriend.q7, newFriend.q8, newFriend.q9, newFriend.q10);
+      friendDBActions.addToDB(
+        newFriend.friend_name, 
+        newFriend.friend_pic, 
+        newFriend.q1,  
+        newFriend.q2, 
+        newFriend.q3,  
+        newFriend.q4, 
+        newFriend.q5, 
+        newFriend.q6, 
+        newFriend.q7, 
+        newFriend.q8, 
+        newFriend.q9, 
+        newFriend.q10);
     }
   });
 };
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //LOAD MIDDLEWARE (BODYPARSER)
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + "/public")));
 
