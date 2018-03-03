f9b78c54d738debb2bbf1fff9fb41a3d4be9ecae
Bob Brown <robertbernardbrown@gmail.com>
2018-03-02 20:36:27 -0500

    trying to help app stay up without crashing on heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express    = require("express");
 const bodyParser = require("body-parser");
-var path         = require("path");
+const path         = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 //LOAD MIDDLEWARE (BODYPARSER)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,4 +13,6 @@ app.use(express.static(path.join(__dirname + "/public")));
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT);
+app.listen(port, function() {
+  console.log("App is running on port " + port);
+});
