15fe41c4b7210b3764e0fa4691731310b2a8f69f
Bob Brown <robertbernardbrown@gmail.com>
2018-03-02 20:24:46 -0500

    removes console log from server and adds node and npm versions to package.json

--- server.js
+++ server.js
@@ -13,6 +13,4 @@ app.use(express.static(path.join(__dirname + "/public")));
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT);
