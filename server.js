d464ab6c1ef34a6555bc7acc59761b6b9d833d77
Bob Brown <robertbernardbrown@gmail.com>
2018-03-02 20:48:24 -0500

    trying to fix

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express    = require("express");
 const bodyParser = require("body-parser");
-const path         = require("path");
+const path       = require("path");
 
 const app = express();
 var port = process.env.PORT || 3000;
 
 //LOAD MIDDLEWARE (BODYPARSER)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + "/public")));
 
