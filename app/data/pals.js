31a0e9645d5630dec3de676f3fdbb244cd605b5a
Bob Brown <robertbernardbrown@gmail.com>
2018-03-03 01:39:03 -0500

    fixes pool queries in db logic. Works

--- app/data/pals.js
+++ app/data/pals.js
@@ -1,5 +1,5 @@
 const mysql      = require("mysql");
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host     : "us-cdbr-iron-east-05.cleardb.net",
   user     : "bfb9298fa194ff",
   password : "9be68183",
@@ -10,29 +10,21 @@ const connection = mysql.createPool({
 
 module.exports = {
 
-  connect: connection.getConnection(err=> {
-    if (err) throw err;
-  }),
-
   displayPalData: function displayPalData (callback) {
-    connection.getConnection(err=> {
+    let palArr=[];
+    pool.query("SELECT * FROM pal JOIN pal_responses ON pal_responses.pal_id = pal.pal_id", (err, res) => {
       if (err) throw err;
-      let palArr=[];
-      connection.query("SELECT * FROM pal JOIN pal_responses ON pal_responses.pal_id = pal.pal_id", (err, res) => {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-          var element = res[i];
-          palArr.push(element);
-        }
-        callback(palArr);
-        connection.releaseConnection();
-      });
+      for (var i = 0; i < res.length; i++) {
+        var element = res[i];
+        palArr.push(element);
+      }
+      callback(palArr);
     });
   },
 
   displayMatchPalData: function displayMatchPalData (callback, index) {
     let palArr=[];
-    connection.query("SELECT * FROM pal JOIN pal_responses ON pal_responses.pal_id = pal.pal_id WHERE pal.pal_id = ?",[index], (err, res) => {
+    pool.query("SELECT * FROM pal JOIN pal_responses ON pal_responses.pal_id = pal.pal_id WHERE pal.pal_id = ?",[index], (err, res) => {
       if (err) throw err;
       for (var i = 0; i < res.length; i++) {
         var element = res[i];
@@ -43,24 +35,23 @@ module.exports = {
   },
 
   fetchPalResponses: function fetchPalResponses(callback) {
-    connection.getConnection(err=> {
+    pool.query("SELECT * FROM pal_responses", (err, res) => {
       if (err) throw err;
-      connection.query("SELECT * FROM pal_responses", (err, res) => {
-        if (err) throw err;
-        callback(res);
-      });
+      callback(res);
     });
   },
 
   addToDB: function addToDb(palName, palPic, answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10) {
-    connection.query("INSERT INTO pal SET pal_name=?, pal_pic=?",
-      [palName, palPic], err => {
-        if (err) throw err;
-      });
-    connection.query("INSERT INTO pal_responses SET question_01=?, question_02=?, question_03=?, question_04=?, question_05=?, question_06=?, question_07=?, question_08=?, question_09=?, question_10=?",
-      [answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10], err => {
-        if (err) throw err;
-      });
-    connection.releaseConnection();
+    pool.getConnection(function (err, connection) {
+      connection.query("INSERT INTO pal SET pal_name=?, pal_pic=?",
+        [palName, palPic], err => {
+          if (err) throw err;
+        });
+      connection.query("INSERT INTO pal_responses SET question_01=?, question_02=?, question_03=?, question_04=?, question_05=?, question_06=?, question_07=?, question_08=?, question_09=?, question_10=?",
+        [answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10], err => {
+          if (err) throw err;
+        });
+      connection.release();
+    });
   }
 };
